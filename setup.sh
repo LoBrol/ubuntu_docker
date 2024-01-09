7546e4df0fbc9f40c76eecc1cd11c683afd3327b
LoBrol <andreabrollo047@gmail.com>
2024-01-10 00:56:50 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -51,7 +51,7 @@ sudo apt -y install apt-transport-https ca-certificates software-properties-comm
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu mantic stable"
 apt-cache policy docker-ce
-sudo apt install docker-ce
+sudo apt install -y docker-ce
 
 
 
